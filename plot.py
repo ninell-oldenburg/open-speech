d5c36455da37367702de2c355827dfeb038f8a44
Ninell Oldenburg <55545843+ninell-oldenburg@users.noreply.github.com>
2020-01-29 11:44:49 +0100

    Update plot.py

--- plot.py
+++ plot.py
@@ -12,6 +12,10 @@ import numpy as np
 from analysis import Main
 
 
+# class for plotting data using matplotlib
+# input: object with the arrays of pitch, quality, text, and decibel
+# as well as array of array of f1 and f2, respectively
+# plus duration
 class Plotting(object):
 
     def __init__(self, sound):
@@ -20,26 +24,31 @@ class Plotting(object):
         self.quality = sound.quality
         self.text = sound.text
 
+        # make sure to not have nan values before computing
         for value in self.pitch:
             np.nan_to_num(value)
 
         self.db = sound.db
         self.f1_2 = sound.f1_2
 
+        # the range of the y-axis of the pitch plot depends
+        # on the min and max values of the pitch
         self.pitch_min = np.nanmin(self.pitch[np.nonzero(self.pitch)])
         self.pitch_max = np.nanmax(self.pitch)
-        print(type(self.pitch_min))
+
+        # there is no text if there is no pitch
         if np.isnan(self.pitch_min):
             self.text = np.array([' ' for _ in range(len(self.text))])
             self.pitch_min = 0
             self.pitch_max = 0
 
-        # set up the figure, the axis, and the subplot elements
+        # set up the figure and the subplot elements
         self.fig = plt.figure()
         ax1 = self.fig.add_subplot(2, 1, 1)
         ax2 = self.fig.add_subplot(2, 2, 3)
         ax3 = self.fig.add_subplot(2, 2, 4)
 
+        # set up axes of the first figure: pitch
         ax1.set_xlim(0,self.dura)
         print('limits: ' + str(self.pitch_min - 50) + ", "+ str(self.pitch_max + 50))
         ax1.set_ylim(self.pitch_min - 50, self.pitch_max + 50)
@@ -51,10 +60,13 @@ class Plotting(object):
         plt.rcParams['font.sans-serif'] = ['Lucida Grande'] + plt.rcParams['font.sans-serif']
         plt.rcParams['mathtext.default'] = 'regular'
 
+        # set up the text
+        # for now it's just normal distributed over the range of time
         self.string = ''
         self.label = ax1.text(self.dura/2, self.pitch_max, self.string, ha='center', color='xkcd:white', va='center', fontsize=12)
         self.scatter = ax1.scatter([], [], s=30, c='xkcd:white', linewidths=1, marker='.')
 
+        # second figure: decibel in form of a bar
         ax2.set_xlim(0, 2)
         ax2.set_ylim(0, np.nanmax(self.db))
         ax2.set_ylabel('Dezibel')
@@ -65,6 +77,7 @@ class Plotting(object):
         y = [1, 2, 3]
         self.bar = ax2.bar(x, y, color='xkcd:white')
 
+        # third: voice quality in form of colormesh
         ax3.set_xlim(0, 1)
         ax3.set_ylim(0, 1)
         ax3.set_facecolor('xkcd:navy')
@@ -73,7 +86,6 @@ class Plotting(object):
 
         # example of circle implementation from:
         # https://brushingupscience.com/2016/06/21/matplotlib-animations-the-easy-way/
-
         u = np.linspace(1, 0, 91)
         v = np.linspace(1, 0, 91)
         w = self.quality
@@ -88,12 +100,13 @@ class Plotting(object):
         self.scatter.set_offsets([])
         self.bar[0].set_height(0)
         self.label.set_text([])
-
         return self.scatter,
 
-    # animation function.  This is called sequentially
+    # animation function. this is called sequentially
+    # defines for every frame i and array of length i
+    # for pitch, db and quality to be rendered
+    # the text works simliar
     def animate(self, i):
-
         y = self.pitch
         x = np.linspace(0, self.dura, len(y))
         data = np.hstack((x[:i, np.newaxis], y[:i, np.newaxis]))
@@ -116,8 +129,7 @@ class Plotting(object):
         return self.scatter,
 
     def plot_this_fig(self):
-        # call the animator.  blit=True means only re-draw the parts that have changed.
-
+        # call the animator. blit=True means only re-draw the parts that have changed.
         interval = (self.dura * 1000)/len(self.pitch)
 
         anim = animation.FuncAnimation(self.fig, self.animate, init_func=self.init,
@@ -133,12 +145,6 @@ class Plotting(object):
         # play.Play('audio/busch/ballade1.wav')
         plt.show()
 
-        """
-        x = threading.Thread(target=play.Play, args=(,))
-        y = threading.Thread(target=plt.show, args=())
-        x.start()
-        y.start()
-        """
 
 if __name__ == '__main__':
     ana = Main('nonblocking.wav', 'Ballade an der Reichstag 1')
